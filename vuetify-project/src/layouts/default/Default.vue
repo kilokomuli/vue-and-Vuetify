54713600db9024034f6369e7bd723e2dfba42830
kilokomuli <kilokoemma175@gmail.com>
2023-11-04 13:29:11 +0300

    layout and props

---
+++ vuetify-project/src/layouts/default/Default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <!-- props :color :elavation :density :location -->
+    <v-app-bar title="Application bar"
+    :color="'red'"
+    :density="'default'"
+    :location="'top'">
+
+    </v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      Main Content
+    </v-main>
+  </v-layout>
+</template>
