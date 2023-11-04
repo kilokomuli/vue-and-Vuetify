a50c836827ee74dd8d727b98460a16c936c349b5
kilokomuli <kilokoemma175@gmail.com>
2023-11-04 15:15:31 +0300

    navigatio drawer

--- vuetify-project/src/layouts/default/Default.vue
+++ vuetify-project/src/layouts/default/Default.vue
@@ -1,21 +1,35 @@
 <template>
   <v-layout class="rounded rounded-md">
     <!-- props :color :elavation :density :location -->
-    <v-app-bar title="Application bar"
-    :color="'red'"
-    :density="'default'"
-    :location="'top'">
-
+    <v-app-bar title="Application bar">
+      <template #append>
+        <div>
+          <v-menu>
+            <template v-slot:activator="{ props }">
+              <v-avatar color="info" v-bind="props">
+                <v-icon icon="mdi-account-circle"></v-icon>
+              </v-avatar>
+            </template>
+            <v-list>
+              <v-list-item v-for="(item, index) in ['Profile', 'Settings', 'Log out']" :key="index" :value="index">
+                <v-list-item-title>{{ item }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+      </template>
     </v-app-bar>
 
-    <v-navigation-drawer>
-      <v-list>
-        <v-list-item title="Navigation drawer"></v-list-item>
+    <v-navigation-drawer expand-on-hover rail>
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
       </v-list>
     </v-navigation-drawer>
 
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-      Main Content
+      <router-view></router-view>
     </v-main>
   </v-layout>
 </template>
--- vuetify-project/src/router/index.ts
+++ vuetify-project/src/router/index.ts
@@ -14,6 +14,22 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: '/posts',
+        name: 'Posts',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Posts.vue'),
+      },
+      {
+        path: '/comments',
+        name: 'Comments',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Comments.vue'),
+      },
     ],
   },
 ]
