6cf29a65c795d0caa5a1c6cf1a612f874cc5b8ae
kilokomuli <kilokoemma175@gmail.com>
2023-11-06 15:41:50 +0300

    vue router installation

---
+++ vue-router/vue-ttavell-app/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="about">
+        <h2>About us</h2>
+    </div>
+</template>
---
+++ vue-router/vue-ttavell-app/src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="home">
+        <h1>
+            Welcome to my website
+        </h1>
+    </div>
+</template>
