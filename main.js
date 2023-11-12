9d2f0d6bb1cdf65401305574fab72a2c059db75d
emma <87370159+kilokomuli@users.noreply.github.com>
2023-11-12 18:14:24 +0300

    creating app introduction to view

---
+++ main.js
@@ -0,0 +1,11 @@
+// creating a vue app
+// {} option object
+const app = Vue.createApp ({
+    // as an argument we pass object and add a data property 
+    data() {
+        return {
+            product : 'Socks',
+            description : 'This are my socks'
+        }
+    },
+});
